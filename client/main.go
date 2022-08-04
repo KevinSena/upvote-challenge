2527ae917cc2ff7a525bbc5562ebd5da130d22c9
Kevin Sena <senakevin50@gmail.com>
2022-08-04 18:43:38 -0300

    style: move client from go path

---
+++ client/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/KevinSena/upvote-challenge/client/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- go/client/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/KevinSena/upvote-challenge/go/client/cmd"
-
-func main() {
-	cmd.Execute()
-}
